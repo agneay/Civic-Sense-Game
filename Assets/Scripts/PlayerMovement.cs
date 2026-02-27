6825071aeb2fb76cd103cca50414cd16e70ebe62
Agneay B Nair <agneaybnair@gmail.com>
2026-02-27 09:00:57 +0530

    fixed animation controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public int maxHealth = 20;
 
     private Rigidbody rb;
+    private Animator animator;
+
     private bool isGrounded;
+
     public HealthBarScript healthBarScript;
     public int currentHealth;
     public QuestBookBehaviour questBookBehaviour;
@@ -15,26 +18,27 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
+
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true; // Prevents player from falling over
+        animator = GetComponent<Animator>();
+
+        rb.freezeRotation = true;
+
         ToastNotification.Show("Hey, Civic Guardian!");
         ToastNotification.Show("Your time is ticking!");
         ToastNotification.Show("Meet, the police officer to know more");
     }
-    void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        healthBarScript.setHealth(currentHealth);
-        ToastNotification.Show("Oops! You took in some damage");
-    }
+
     void Update()
     {
         MovePlayer();
+        HandleAnimations();
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
         }
+
         if (Input.GetKeyDown(KeyCode.O))
         {
             questBookBehaviour.OpenQuestBook();
@@ -43,19 +47,44 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        float x = Input.GetAxis("Horizontal"); // A / D
-        float z = Input.GetAxis("Vertical");   // W / S
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-        Vector3 velocity = new Vector3(move.x * moveSpeed, rb.linearVelocity.y, move.z * moveSpeed);
+        Vector3 velocity = new Vector3(
+            move.x * moveSpeed,
+            rb.linearVelocity.y,
+            move.z * moveSpeed
+        );
 
         rb.linearVelocity = velocity;
     }
 
+    void HandleAnimations()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        bool isMoving = (Mathf.Abs(x) > 0.1f || Mathf.Abs(z) > 0.1f);
+        bool isRunning = Input.GetKey(KeyCode.LeftShift) && isMoving;
+
+        animator.SetBool("Walking", isMoving && !isRunning);
+        animator.SetBool("Running", isRunning);
+    }
+
     void Jump()
     {
-        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        isGrounded = false;
+
+        rb.linearVelocity = new Vector3(
+            rb.linearVelocity.x,
+            0f,
+            rb.linearVelocity.z
+        );
+
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        animator.SetTrigger("Jump");
     }
 
     private void OnCollisionStay(Collision collision)
@@ -66,5 +95,12 @@ public class PlayerMovement : MonoBehaviour
     private void OnCollisionExit(Collision collision)
     {
         isGrounded = false;
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBarScript.setHealth(currentHealth);
+        ToastNotification.Show("Oops! You took in some damage");
     }
 }
